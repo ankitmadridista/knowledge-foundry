3927b20f415e2dc7439955fbc98ba780fff5993a
ankitmadridista <ankit.suravkar4@gmail.com>
2026-08-02 19:42:51 +0530

    feat(domain): add domain event collection to entity

--- apps/api/src/KnowledgeFoundry.Domain/Common/Base/Entity.cs
+++ apps/api/src/KnowledgeFoundry.Domain/Common/Base/Entity.cs
@@ -1,9 +1,26 @@
+using KnowledgeFoundry.Domain.Common.Events;
 public abstract class Entity
 {
     public Guid Id { get; protected set; }
 
+    private readonly List<IDomainEvent> _domainEvents = [];
+
+    public IReadOnlyCollection<IDomainEvent> DomainEvents
+    => _domainEvents.AsReadOnly();
+
     protected Entity()
     {
         Id = Guid.NewGuid();
+    }
+
+    protected void RaiseDomainEvent(
+    IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
     }
 }
